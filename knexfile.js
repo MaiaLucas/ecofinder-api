0957a9e910b7169579ffc31e44c8eac2fce24965
MaiaLucas <barretomaia5466@outlook.com>
2020-08-24 21:25:31 -0300

    mudando start no heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,6 @@
 module.exports = {
   client: "pg",
-  connection: {
-    url: process.env.DATABASE_URL,
-    charset: "utf8",
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
     tableName: "knex_migrations",
   },
