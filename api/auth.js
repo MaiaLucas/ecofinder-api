9f8da7a3b47a9dca4c179c4aae90a835ce6851e0
MaiaLucas <barretomaia5466@outlook.com>
2020-08-24 21:32:48 -0300

    alteração no auth_secret

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,3 @@
-const { authSecret } = require("../.env");
 const jwt = require("jwt-simple");
 const bcrypt = require("bcrypt-nodejs");
 
@@ -27,7 +26,7 @@ module.exports = (app) => {
 
     res.json({
       ...payload,
-      token: jwt.encode(payload, authSecret),
+      token: jwt.encode(payload, process.env.AUTH_SECRET),
     });
   };
 
@@ -35,7 +34,7 @@ module.exports = (app) => {
     const userData = req.body || null;
     try {
       if (userData) {
-        const token = jwt.decode(userData.token, authSecret);
+        const token = jwt.decode(userData.token, process.env.AUTH_SECRET);
         if (new Date(token.exp * 1000) > new Date()) {
           return res.send(true);
         }
