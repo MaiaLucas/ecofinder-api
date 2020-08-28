bd681055b7ae8041a4dc4f57b832e811a405df1e
MaiaLucas <barretomaia5466@outlook.com>
2020-08-27 21:49:51 -0300

    teste de acesso ao banco de dados pelo heroku

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,41 @@
-const { db } = require("./.env.develop");
-module.exports = {
-  development: {
-    client: "pg",
-    connection: db,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./seeds",
-    },
-  },
+const { db } = process.env.DATABASE_URL || require("./.env.develop");
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: "knex_migrations",
-    },
-    // seeds: {
-    //   directory: "./seeds",
-    // },
+module.exports = {
+  client: "postgresql",
+  connection: db,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
   },
 };
+
+// module.exports = {
+//   // development: {
+//   client: "postgresql",
+//   connection: db,
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   },
+//   migrations: {
+//     tableName: "knex_migrations",
+//   },
+//   seeds: {
+//     directory: "./seeds",
+//   },
+//   // },
+
+//   // production: {
+//   //   client: "pg",
+//   //   connection: process.env.DATABASE_URL,
+//   //   migrations: {
+//   //     tableName: "knex_migrations",
+//   //   },
+//   //   // seeds: {
+//   //   //   directory: "./seeds",
+//   //   // },
+//   // },
+// };
