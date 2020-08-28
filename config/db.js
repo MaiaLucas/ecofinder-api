756223f88f596630eafebd7d795fcb2c12c0c173
MaiaLucas <barretomaia5466@outlook.com>
2020-08-27 22:00:31 -0300

    ajuste de acesso ao banco de dados

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const environment = process.env.ENVIRONMENT || "development";
 console.log(environment);
-const config = require("../knexfile.js"); //[environment];
+const config = require("../knexfile.js")[environment];
 const knex = require("knex")(config);
 
 // knex.migrate.latest([config]);
--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,29 @@
-const { db } = process.env.DATABASE_URL || require("./.env.develop");
+const { db } = require("./.env.develop");
 
 module.exports = {
-  client: "postgresql",
-  connection: db,
-  pool: {
-    min: 2,
-    max: 10,
+  development: {
+    client: "postgresql",
+    connection: db,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
-  migrations: {
-    tableName: "knex_migrations",
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    // seeds: {
+    //   directory: "./seeds",
+    // },
   },
 };
-
-// module.exports = {
-//   // development: {
-//   client: "postgresql",
-//   connection: db,
-//   pool: {
-//     min: 2,
-//     max: 10,
-//   },
-//   migrations: {
-//     tableName: "knex_migrations",
-//   },
-//   seeds: {
-//     directory: "./seeds",
-//   },
-//   // },
-
-//   // production: {
-//   //   client: "pg",
-//   //   connection: process.env.DATABASE_URL,
-//   //   migrations: {
-//   //     tableName: "knex_migrations",
-//   //   },
-//   //   // seeds: {
-//   //   //   directory: "./seeds",
-//   //   // },
-//   // },
-// };
