c90a4c016145e7025698df844f7f2fd06774ddfb
MaiaLucas <barretomaia5466@outlook.com>
2020-08-29 11:27:58 -0300

    tentando conectar com o banco de produção

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,14 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_ACCESS_KEY,
+    },
+    // connection: process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
     },
