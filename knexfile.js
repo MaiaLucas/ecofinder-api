f654f7e70f6ebcedac6c0885df093fe7c615585f
MaiaLucas <barretomaia5466@outlook.com>
2020-08-29 11:40:11 -0300

    tentando conectar com o banco de produção

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 // const { db } = require("./.env.develop");
 require("dotenv").config();
-console.log(process.env.DATABASE_URL);
+
 module.exports = {
   development: {
     client: "pg",
@@ -25,19 +25,12 @@ module.exports = {
 
   production: {
     client: "pg",
-    // connection: {
-    //   port: process.env.DB_PORT,
-    //   host: process.env.DB_HOST,
-    //   database: process.env.DB_NAME,
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_ACCESS_KEY,
-    // },
     connection: process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
     },
-    // seeds: {
-    //   directory: "./seeds",
-    // },
+    seeds: {
+      directory: "./seeds",
+    },
   },
 };
