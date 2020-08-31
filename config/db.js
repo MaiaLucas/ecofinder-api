7777c76774d19343c211c2102399192fe503ae9d
MaiaLucas <barretomaia5466@outlook.com>
2020-08-31 19:23:16 -0300

    remoção dos console.log

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 const environment = process.env.ENVIRONMENT || "development";
-console.log("environment -> ", environment);
-console.log("database -> ", process.env.DATABASE_URL);
 const config = require("../knexfile.js")[environment];
 const knex = require("knex")(config);
-console.log(knex);
 // knex.migrate.latest([config]);
 module.exports = knex;
