d5aba226ebd24a6d7f4b2eb9e47adca17e7210b0
MaiaLucas <barretomaia5466@outlook.com>
2020-09-09 01:06:01 -0300

    adicionando uma nova coluna

--- migrations/20200818225336_create_table_information.js
+++ migrations/20200818225336_create_table_information.js
@@ -3,6 +3,7 @@ exports.up = function (knex) {
     table.increments("id").primary();
     table.string("title").notNull();
     table.string("description", 1000).notNull();
+    table.binary("content").notNull();
     table.string("image_url", 1000);
     table.dateTime("create_at").notNull();
     table.dateTime("update_at").notNull();
