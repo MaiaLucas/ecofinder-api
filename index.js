c013b2837873dafe2aa46413214a655c4041a341
MaiaLucas <barretomaia5466@outlook.com>
2021-02-02 21:05:38 -0300

    mudança da porta

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const app = express();
 
 const bucketName = "ecofinder-api-images-e28841c6-b9c1-4675-baa7-a0d2a11151a1";
 AWS.config.getCredentials(function (err) {
-	console.log("has credentials");
+  console.log("has credentials");
 });
 
 app.db = db; //knex
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 8888;
 consign()
-	.include("./config/passport.js")
-	.then("./config/middlewares.js")
-	.then("./api/validation.js")
-	.then("./api")
-	.then("./config/routes.js")
-	.into(app);
+  .include("./config/passport.js")
+  .then("./config/middlewares.js")
+  .then("./api/validation.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
 
 app.get("/", (req, res) => {
-	res.send("<h1>Master working</h1>");
+  res.send("<h1>Master working</h1>");
 });
 
 app.use(express.json());
@@ -30,5 +30,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.listen(port, () => {
-	console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
