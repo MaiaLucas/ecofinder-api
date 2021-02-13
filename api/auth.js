b932acc2e888ee724cf0cd857c94cbfd6301f0f7
MaiaLucas <barretomaia5466@outlook.com>
2021-02-13 18:08:17 -0300

    remoção da propriedade code no response

--- api/auth.js
+++ api/auth.js
@@ -3,54 +3,49 @@ const bcrypt = require("bcrypt-nodejs");
 require("dotenv").config();
 
 module.exports = (app) => {
-	const signin = async (req, res) => {
-		if (!req.body.email || !req.body.password)
-			return res
-				.status(400)
-				.json({ code: 400, message: "Favor informar email e senha!" });
+  const signin = async (req, res) => {
+    console.log(req.body);
+    if (!req.body.email || !req.body.password)
+      return res.status(400).json({ message: "Favor informar email e senha!" });
 
-		const user = await app.db("users").where({ email: req.body.email }).first();
+    const user = await app.db("users").where({ email: req.body.email }).first();
 
-		if (!user)
-			return res
-				.status(400)
-				.json({ code: 400, message: "Usuário não encontrado" });
+    if (!user)
+      return res.status(400).json({ message: "Usuário não encontrado" });
 
-		const isMatch = bcrypt.compareSync(req.body.password, user.password);
+    const isMatch = bcrypt.compareSync(req.body.password, user.password);
 
-		if (!isMatch)
-			return res
-				.status(401)
-				.json({ code: 401, message: "Email/Senha inválidos" });
+    if (!isMatch)
+      return res.status(401).json({ message: "Email/Senha inválidos" });
 
-		const now = Math.floor(Date.now() / 1000);
+    const now = Math.floor(Date.now() / 1000);
 
-		const payload = {
-			id: user.id,
-			username: user.username,
-			email: user.email,
-			iat: now,
-			exp: now + 60 * 60 * 24,
-		};
+    const payload = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      iat: now,
+      exp: now + 60 * 60 * 24,
+    };
 
-		res.json({
-			...payload,
-			token: jwt.encode(payload, process.env.AUTH_SECRET),
-		});
-	};
+    res.json({
+      ...payload,
+      token: jwt.encode(payload, process.env.AUTH_SECRET),
+    });
+  };
 
-	const validateToken = async (req, res) => {
-		const userData = req.body || null;
-		try {
-			if (userData) {
-				const token = jwt.decode(userData.token, process.env.AUTH_SECRET);
-				return res.send(new Date(token.exp * 1000) > new Date());
-			}
-		} catch (e) {
-			//problema com o token
-			return false; //res.status(400).json(false);
-		}
-	};
+  const validateToken = async (req, res) => {
+    const userData = req.body || null;
+    try {
+      if (userData) {
+        const token = jwt.decode(userData.token, process.env.AUTH_SECRET);
+        return res.send(new Date(token.exp * 1000) > new Date());
+      }
+    } catch (e) {
+      //problema com o token
+      return false; //res.status(400).json(false);
+    }
+  };
 
-	return { signin, validateToken };
+  return { signin, validateToken };
 };
