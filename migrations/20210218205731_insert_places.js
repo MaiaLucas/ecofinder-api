65ad80c4651453baa276f4dc6dcc2b41048da6c3
MaiaLucas <barretomaia5466@outlook.com>
2021-02-22 11:25:48 -0300

    feat: migrate de insert nas tabelas

---
+++ migrations/20210218205731_insert_places.js
@@ -0,0 +1,47 @@
+exports.up = function (knex) {
+  return knex("places").insert([
+    {
+      title: "Ecoponto",
+      description: "leve seu lixo",
+      images_url: "",
+      city: "Fortaleza",
+      rating: 4.87,
+      address: "Rua dos bobos, 0",
+      phone: "(99) 999999999",
+      open_on_weekend: true,
+      create_at: new Date(),
+      update_at: new Date(),
+      type: 1,
+    },
+    {
+      title: "Ecopoint",
+      description: "Passeio bacana",
+      images_url: "",
+      city: "Fortaleza",
+      rating: 4.57,
+      address: "Rua dos bobos, 0",
+      phone: "(99) 999999999",
+      open_on_weekend: true,
+      create_at: new Date(),
+      update_at: new Date(),
+      type: 2,
+    },
+    {
+      title: "Passeio no Parque do Cocó",
+      description: "muito sustentável",
+      images_url: "",
+      city: "Fortaleza",
+      rating: 4.92,
+      address: "Rua dos bobos, 0",
+      phone: "(99) 999999999",
+      open_on_weekend: true,
+      create_at: new Date(),
+      update_at: new Date(),
+      type: 2,
+    },
+  ]);
+};
+
+exports.down = function (knex) {
+  return knex("places").del();
+};
---
+++ migrations/20210218211730_insert_products.js
@@ -0,0 +1,40 @@
+exports.up = function (knex) {
+  return knex("products").insert([
+    {
+      title: "Escova de bambu",
+      images_url: "",
+      instagram_account: "@minhaloja",
+      rating: 4.87,
+      create_at: new Date(),
+      update_at: new Date(),
+    },
+    {
+      title: "Ecogarrafinha",
+      images_url: "",
+      instagram_account: "@minhaloja",
+      rating: 4.7,
+      create_at: new Date(),
+      update_at: new Date(),
+    },
+    {
+      title: "Ecobag",
+      images_url: "",
+      instagram_account: "@minhaloja",
+      rating: 5.0,
+      create_at: new Date(),
+      update_at: new Date(),
+    },
+    {
+      title: "Canudo de bambu",
+      images_url: "",
+      instagram_account: "@minhaloja",
+      rating: 4.5,
+      create_at: new Date(),
+      update_at: new Date(),
+    },
+  ]);
+};
+
+exports.down = function (knex) {
+  return knex("products").del();
+};
