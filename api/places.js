b05672fb35439dc908ce61dd42ee4b2949450421
MaiaLucas <barretomaia5466@outlook.com>
2021-02-22 22:17:44 -0300

    fix: refazendo a api de listagem

--- api/places.js
+++ api/places.js
@@ -4,23 +4,18 @@ module.exports = (app) => {
   const { isEmpty } = app.api.validation;
 
   /**
-   * função responsável por cadastrar/editar um local
+   * função responsável por cadastrar um local
    */
-  const save = (req, res) => {
+  function create(req, res) {
     const place = { ...req.body };
 
-    if (req.params.id) place.id = req.params.id;
     try {
-      isEmpty(place.title, "Campo Título é obrigatório");
-      isEmpty(place.state, "Campo Estado é obrigatório");
-      isEmpty(place.city, "Campo Cidade é obrigatório");
-      isEmpty(place.phone, "Campo Telefone é obrigatório");
-      isEmpty(place.address, "Campo Endereço é obrigatório");
-      isEmpty(place.hr_init, "Campo Hora Inicial é obrigatório");
-      isEmpty(place.hr_final, "Campo Hora Final é obrigatório");
-      isEmpty(place.opening_days, "Campo Dias de Funcionamento é obrigatório");
-      isEmpty(place.author, "Campo Autor é obrigatório");
-      isEmpty(place.type, "Campo Type é obrigatório");
+      isEmpty(title, "Campo Título é obrigatório");
+      isEmpty(city, "Campo Cidade é obrigatório");
+      isEmpty(address, "Campo Endereço é obrigatório");
+      isEmpty(phone, "Campo Telefone é obrigatório");
+      isEmpty(hr_init, "Campo Hora Inicial é obrigatório");
+      isEmpty(hr_final, "Campo Hora Final é obrigatório");
     } catch (msg) {
       return res.status(400).json({ message: msg });
     }
@@ -33,195 +28,161 @@ module.exports = (app) => {
 
     place.images_url = req.files.length ? images.join(",") : place.images;
 
-    if (place.id) {
-      place.update_at = new Date(Date.now());
+    place.create_at = new Date(Date.now());
+    place.update_at = new Date(Date.now());
 
-      app
-        .db("places")
-        .update(place)
-        .where({ id: place.id })
-        .then((_) =>
-          res.status(200).json({ message: "Local alterado com sucesso!" })
-        )
-        .catch((err) => res.status(500).send(err));
-    } else {
-      place.create_at = new Date(Date.now());
-      place.update_at = new Date(Date.now());
-
-      app
-        .db("places")
-        .insert(place)
-        .then((_) =>
-          res.status(200).json({ message: "Local cadastrado com sucesso!" })
-        )
-        .catch((err) => res.status(500).send({ message: err }));
-    }
-  };
-
-  const listAll = (req, res) => {
     app
       .db("places")
-      .select("*")
-      .then((places) => res.json(places))
-      .catch((err) => res.status(500).send(err));
-  };
+      .insert(place)
+      .then((_) =>
+        res.status(200).json({ message: "Local cadastrado com sucesso!" })
+      )
+      .catch((err) =>
+        res.status(500).send({ message: "Internal Server Error" })
+      );
+  }
 
   /**
-   *  Listagem por classificação
+   * função responsável por editar um local
    */
-  const listByRating = (req, res) => {
-    app
-      .db("places")
-      .select("title", "rating", "images_url as mainImage")
-      .orderBy("rating", "desc")
-      .then((places) => res.json(places))
-      .catch((err) => res.status(500).send({ message: err }));
-  };
+  function edit(req, res) {
+    const place = { ...req.body };
 
-  const listByType = (req, res) => {
-    app
-      .db("places")
-      .select("*")
-      .where({ type: req.params.id })
-      .then((places) => res.json(places))
-      .catch((err) => res.status(500).send(err));
-  };
+    if (!req.params.id)
+      return res.status(400).json({ message: "Local não encontrado" });
 
-  const listById = (req, res) => {
-    console.log("object");
+    const { title, city, address, phone, hr_init, hr_final } = place;
+
+    try {
+      isEmpty(title, "Campo Título é obrigatório");
+      isEmpty(city, "Campo Cidade é obrigatório");
+      isEmpty(address, "Campo Endereço é obrigatório");
+      isEmpty(phone, "Campo Telefone é obrigatório");
+      isEmpty(hr_init, "Campo Hora Inicial é obrigatório");
+      isEmpty(hr_final, "Campo Hora Final é obrigatório");
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+
+    const requestImages = req.files;
+
+    const images = requestImages.map((image) => {
+      return image.location;
+    });
+
+    place.images_url = req.files.length ? images.join(",") : place.images;
+
+    place.update_at = new Date(Date.now());
+
     app
       .db("places")
-      .select("*")
+      .update(place)
       .where({ id: req.params.id })
-      .first()
-      .then((places) => {
-        console.log(places);
-        res.json(places);
-      })
-      .catch((err) => res.status(500).send(err));
-  };
+      .then((_) =>
+        res.status(200).json({ message: "Local alterado com sucesso!" })
+      )
+      .catch((err) =>
+        res.status(500).send({ message: "Internal Server Error" })
+      );
+  }
 
-  const listImagesById = (req, res) => {
-    app
-      .db("places")
-      .select("images_url")
-      .where({ id: req.params.id })
-      .first()
-      .then((places) => res.json(places))
-      .catch((err) => res.status(500).send(err));
-  };
-
-  const listByLocal = async (req, res) => {
-    const places = await app.db.raw(
-      `
-        SELECT * 
-        FROM places 
-        WHERE 
-          LOWER(city) LIKE LOWER('%${req.params.place.toLowerCase()}%')
-          or LOWER(state) LIKE LOWER('%${req.params.place.toLowerCase()}%')
-      `
-    );
-    const ids = places.rows.map((c) => c.id);
-
-    app
-      .db("places")
-      .whereIn("id", ids)
-      .orderBy("type", "asc")
-      .then((places) => {
-        let garbage = [];
-        let experience = [];
-        let store = [];
-        places.forEach((place) => {
-          if (place.type === 1) {
-            garbage.push(place);
-          } else if (place.type === 2) {
-            experience.push(place);
-          } else {
-            store.push(place);
-          }
-        });
-        res.json([garbage, experience, store]);
-      })
-      .catch((err) => res.status(500).send(err));
-  };
-
-  const listByCity = async (req, res) => {
-    const places = await app.db.raw(
-      `
-		    SELECT city
-		    FROM places
-		    WHERE
-		      LOWER(city) LIKE LOWER('%${req.params.city.toLowerCase()}%')
-		    GROUP BY city
-		  `
-    );
-
-    const cities = places.rows.map((c) => c.city);
-    res.send(cities);
-    // app
-    // 	.db("places")
-    // 	.whereIn("city", cities)
-    // 	.orderBy("city", "asc")
-    // 	.then((places) => {
-    // 		res.json(places);
-    // 	})
-    // 	.catch((err) => res.status(500).send(err));
-  };
-
-  const listByLocalType = async (req, res) => {
-    const places = await app.db.raw(
-      `
-        SELECT * 
-        FROM places
-        WHERE type = ${req.params.id}
-        ${
-          req.params.place.toLowerCase() != undefined
-            ? `AND (LOWER(city) LIKE LOWER('%${req.params.place.toLowerCase()}%')
-        OR LOWER(state) LIKE LOWER('%${req.params.place.toLowerCase()}%'))`
-            : ""
-        } 
-      `
-    );
-    const ids = places.rows.map((c) => c.id);
-
-    app
-      .db("places")
-      .whereIn("id", ids)
-      .orderBy("create_at", "desc")
-      .then((places) => res.json(places))
-      .catch((err) => res.status(500).send(err));
-  };
-
-  const remove = async (req, res) => {
+  /**
+   * função responsável por deletar um local
+   */
+  async function remove(req, res) {
     try {
       const rowsDeleted = await app
         .db("places")
         .where({ id: req.params.id })
         .del();
 
-      console.log(rowsDeleted);
-      // try {
-      // 	notExistsOrError(rowsDeleted, "Resultado não foi encontrado.");
-      // } catch (msg) {
-      // 	return res.status(400).send(msg);
-      // }
-
       rowsDeleted && res.status(204).send();
     } catch (msg) {
-      res.status(500).send(msg);
+      res.status(500).send({ message: "Internal Server Error" });
     }
-  };
+  }
+
+  /**
+   * função responsável por listar os locais ordenando por classificação,
+   * @param {*} page responsável pelo limite da listagem
+   */
+  function list(req, res) {
+    const page = !req.query.page ? 4 : req.query.page;
+    app
+      .db("places")
+      .select("title", "rating")
+      .from("places")
+      .orderBy("rating", "desc")
+      .limit(page)
+      .then((place) => res.json(place))
+      .catch((err) =>
+        res.status(500).send({ message: "Internal Server Error" })
+      );
+  }
+
+  /**
+   * função responsável por listar os locais ordenando por classificação,
+   * @param {*} city responsável pela busca por cidade
+   * @param {*} type responsável pela filtragem por tipo
+   */
+  async function listByCityType(req, res) {
+    const type = !req.query.type ? "" : req.query.type;
+    const city = !req.query.city ? "" : req.query.city.toLowerCase();
+    const places = await app.db.raw(
+      `
+        SELECT id, title, rating
+        FROM places
+        WHERE 1 = 1
+        ${type ? `AND type = ${type}` : ""}
+        ${city ? `AND (LOWER(city) LIKE LOWER('%${city}%'))` : ""}
+      `
+    );
+    const ids = places.rows.map((c) => c.id);
+
+    app
+      .db("places")
+      .whereIn("id", ids)
+      .orderBy("rating", "desc")
+      .then((places) => res.json(places))
+      .catch((err) =>
+        res.status(500).send({ message: "Internal Server Error" })
+      );
+  }
+
+  /**
+   * função responsável por listar as cidades ao pesquisar
+   */
+  async function autocompleteCities(req, res) {
+    const city = !req.query.city ? "" : req.query.city.toLowerCase();
+    const places = await app.db.raw(
+      `
+        SELECT id
+        FROM places
+        WHERE 1 = 1
+        ${city ? `AND (LOWER(city) LIKE LOWER('%${city}%'))` : ""}
+        LIMIT 3
+      `
+    );
+    const ids = places.rows.map((c) => c.id);
+
+    app
+      .db("places")
+      .distinct("city")
+      .whereIn("id", ids)
+      .groupBy("city")
+      .orderBy("city", "desc")
+      .then((places) => res.json(places))
+      .catch((err) =>
+        res.status(500).send({ message: "Internal Server Error" })
+      );
+  }
 
   return {
-    save,
-    listAll,
-    listByType,
-    listByCity,
-    listById,
-    listByLocal,
-    listByLocalType,
-    listImagesById,
-    listByRating,
+    create,
+    edit,
     remove,
+    list,
+    listByCityType,
+    autocompleteCities,
   };
 };
