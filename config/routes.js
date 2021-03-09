d0da0a535beaf9ede0a5676f066a342367dd90cf
MaiaLucas <barretomaia5466@outlook.com>
2021-03-09 00:37:23 -0300

    adicionando novas rotas

--- config/routes.js
+++ config/routes.js
@@ -15,7 +15,21 @@ module.exports = (app) => {
    * /search_place?city=fortaleza&type=1
    * /autocomplete?city=fortaleza
    */
+  // Exibe os destaques
   app.get("/dashboard", app.api.places.list);
+  // Pesquisa pelos locais
   app.get("/search_place", app.api.places.listByCityType);
-  app.get("/autocomplete", app.api.places.autocompleteCities);
+
+  //Retorna as cidades que estão sendo buscadas
+  app.get("/cities", app.api.places.cities);
+
+  // Cadastro de um ponto de coleta ou experiencia
+  app.post(
+    "/place/create",
+    multer(multerConfig).array("images"),
+    app.api.places.create
+  );
+
+  // Exibe os detalhes de um ponto de coleta ou experiencia
+  app.get("/place/detail/:id", app.api.places.placeDetail);
 };
