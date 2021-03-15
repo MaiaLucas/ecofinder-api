44a8a48a4bb0507b5cc6a85f31ee32f42dd559d9
MaiaLucas <barretomaia5466@outlook.com>
2021-03-15 14:55:57 -0300

    ajuste ssl

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
     migrations: {
       // tableName: "knex_migrations",
       directory: __dirname + "/migrations",
