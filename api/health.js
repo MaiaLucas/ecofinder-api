e63fc1f125a1f500ae0963754c9538fc3ad4721f
MaiaLucas <barretomaia5466@outlook.com>
2021-03-15 17:52:42 -0300

    ajuste health

--- api/health.js
+++ api/health.js
@@ -2,7 +2,8 @@ module.exports = (app) => {
   const work = (req, res) => {
     app.db.context
       .raw("Select 1;")
-      .then((_) => res.status(200).send("conexão feita"));
+      .then((_) => res.status(200).send("conexão feita"))
+      .catch((err) => console.log(err));
   };
 
   return { work };
