0cf72a3af466bc4381cc9c292200acf2a3c063a4
MaiaLucas <barretomaia5466@outlook.com>
2021-04-05 21:57:28 -0300

    ajuste na api de autenticação

--- api/auth.js
+++ api/auth.js
@@ -1,51 +1,91 @@
-const jwt = require("jwt-simple");
+// const jwt = require("jwt-simple");
 const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
 require("dotenv").config();
 
 module.exports = (app) => {
-  const signin = async (req, res) => {
-    console.log(req.body);
+  const login = async (req, res) => {
     if (!req.body.email || !req.body.password)
       return res.status(400).json({ message: "Favor informar email e senha!" });
 
+    const password = crypto
+      .createHash("sha256")
+      .update(req.body.password)
+      .digest("hex");
     const user = await app.db("users").where({ email: req.body.email }).first();
 
+    console.log(user);
     if (!user)
       return res.status(400).json({ message: "Usuário não encontrado" });
 
-    const isMatch = bcrypt.compareSync(req.body.password, user.password);
-
-    if (!isMatch)
-      return res.status(401).json({ message: "Email/Senha inválidos" });
-
-    const now = Math.floor(Date.now() / 1000);
-
     const payload = {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      iat: now,
-      exp: now + 60 * 60 * 24,
+      email: req.body.username,
+      type: "access",
     };
 
-    res.json({
-      ...payload,
-      token: jwt.encode(payload, process.env.AUTH_SECRET),
+    const csrfPayload = {
+      email: req.body.username,
+      type: "csrf",
+    };
+
+    const token = jwt.sign(payload, process.env.AUTH_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "2 days",
     });
+    const csrf = jwt.sign(csrfPayload, process.env.AUTH_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "2 days",
+    });
+
+    console.log("sucesso");
+    const expTime = 2 * 24 * 60 * 60 * 1000;
+    res.cookie("jwt", token, {
+      magAge: expTime,
+      httpOnly: true,
+    });
+    res.json({ token: csrf, user: user.id, expiresIn: expTime });
+
+    // const isMatch = bcrypt.compareSync(req.body.password, user.password);
+
+    // if (!isMatch)
+    //   return res.status(401).json({ message: "Email/Senha inválidos" });
+
+    // const now = Math.floor(Date.now() / 1000);
+
+    // const payload = {
+    //   id: user.id,
+    //   username: user.username,
+    //   email: user.email,
+    //   iat: now,
+    //   exp: now + 60 * 60 * 24,
+    // };
+
+    // res.json({
+    //   ...payload,
+    //   token: jwt.encode(payload, process.env.AUTH_SECRET),
+    // });
   };
 
-  const validateToken = async (req, res) => {
-    const userData = req.body || null;
+  function validate(req, res) {
+    const csrf = req.get("CSRF");
+    const str = req.cookies["jwt"];
+
     try {
-      if (userData) {
-        const token = jwt.decode(userData.token, process.env.AUTH_SECRET);
-        return res.send(new Date(token.exp * 1000) > new Date());
-      }
-    } catch (e) {
-      //problema com o token
-      return false; //res.status(400).json(false);
-    }
-  };
+      let jwtPayload = jwt.verify(str, process.env.AUTH_SECRET);
+      let csrfPayload = jwt.verify(csrf, process.env.AUTH_SECRET);
 
-  return { signin, validateToken };
+      if (jwtPayload["type"] != "access")
+        req.status(400).json({ message: "Invalid jwt payload" });
+      if (jwtPayload["type"] != "csrf")
+        req.status(400).json({ message: "Invalid anti-CSRF token payload" });
+
+      res.status(201).send();
+    } catch (e) {
+      res.status(401).send();
+    }
+  }
+
+  return { login, validate };
 };
