9068359a6d68e3332f9919abcc1eb8f1bb859ed0
MaiaLucas <barretomaia5466@outlook.com>
2021-05-02 10:28:18 -0300

    não utilizando a tabela de rating

--- migrations/20210416202622_create_table_rating.js
+++ migrations/20210416202622_create_table_rating.js
@@ -2,9 +2,9 @@ exports.up = function (knex) {
   return knex.schema.createTable("rating", function (table) {
     table.increments("id");
     table.float("rating");
-    table.string("author").references("id").inTable("users");
-    table.integer("place").references("id").inTable("places");
-    table.integer("product").references("id").inTable("products");
+    // table.string("author").references("id").inTable("users");
+    // table.integer("place").references("id").inTable("places");
+    // table.integer("product").references("id").inTable("products");
 
     table.dateTime("create_at").defaultTo(knex.fn.now()).notNull();
     table.dateTime("update_at").defaultTo(knex.fn.now()).notNull();
